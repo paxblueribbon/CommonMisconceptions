b41f9940809d7aa69dcc9606cd7baa9105a2c251
Paxana <veganbikepunk@gmail.com>
2016-02-19 17:58:58 -0800

    beginning to switch facts to list of common misconceptions

--- app/src/main/java/com/example/paxie/funfacts/FactBook.java
+++ app/src/main/java/com/example/paxie/funfacts/FactBook.java
@@ -10,9 +10,9 @@ public class FactBook {
     // Member variables (properties about the object)
 
     public String[] mFacts = {
-            "Ants stretch when they wake up in the morning.",
-            "Ostriches can run faster than horses.",
-            "Olympic gold medals are actually made mostly of silver.",
+            "Searing meat does not seal in moisture, and in fact may actually cause meat to lose moisture in comparison to an equivalent amount of cooking without searing. Generally, the value in searing meat is that it creates a brown crust with a rich flavor via the Maillard reaction.",
+            "Some people believe that food items cooked with wine or liquor will be totally non-alcoholic, because alcohol's low boiling point causes it to evaporate quickly when heated. However, a study found that some of the alcohol remains: 25 percent after one hour of baking or simmering, and 10 percent after two hours; in either case, however, the amount consumed while eating a dish prepared with alcohol will rarely if ever contain sufficient alcohol to cause even low levels of intoxication.",
+            "Monosodium glutamate (MSG) has a widespread reputation for triggering migraine headache exacerbations and other symptoms of so-called Chinese restaurant syndrome, but there are no consistent data to support this relationship. Although there have been reports of an MSG-sensitive subset of the population, this has not been demonstrated in placebo-controlled trials.",
             "You are born with 300 bones; by the time you are an adult you will have 206.",
             "It takes about 8 minutes for light from the Sun to reach Earth.",
             "Some bamboo plants can grow almost a meter in just one day.",
